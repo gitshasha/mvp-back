23b7b0ebda347252efd2012772b85fbe064dfda9
Shashank_kyadari <74113664+gitshasha@users.noreply.github.com>
2024-07-24 01:14:56 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,16 @@
 const Pool=require("pg").Pool
-const pool=new Pool({
-    host:"localhost",
-    user:"postgres",
-    port:5432,
-    password:"Konnichiwa@123",
-    database:"School_db"
-})
+const pool = new Pool({
+  //   host: "localhost",
+
+  host: "aws-0-ap-south-1.pooler.supabase.com",
+  //   user: "postgres",
+  user: "postgres.evswsuuoxliplhvapvwt",
+  //   port: 5432,
+  port: 6543,
+
+  password: "Konnichiwa@123",
+  //   database: "School_db",
+
+  database: "postgres",
+});
 module.exports=pool; 
