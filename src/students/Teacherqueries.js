898703c216fc11b9176e6cdeb3cdc2f256e97fed
Shashank_kyadari <74113664+gitshasha@users.noreply.github.com>
2024-07-24 03:09:35 +0530

    Update Teacherqueries.js

--- src/students/Teacherqueries.js
+++ src/students/Teacherqueries.js
@@ -1,4 +1,4 @@
-const checkteacher = `select * from teachers where email=$1`;
+const checkteacher = `select * from public.teachers where email=$1`;
 
 const answercheck = `SELECT
     s.student_id,
@@ -12,24 +12,23 @@ const answercheck = `SELECT
         WHEN s.submitted_answer = q.correct_answer THEN 'Correct'
         ELSE 'Incorrect'
     END AS answer_status
-FROM
-    submissions s
+FROM public.submissions s
 JOIN
-    quiz_questions q ON s.question_id = q.question_id
+    public.quiz_questions q ON s.question_id = q.question_id
 where student_id=$1`;
 
 const getteacherclasses = `
 SELECT c.class_subject_id, c.class_id, c.subject_id, c.teacher_id, s.subject_name, t.teacher_name,e.class_name
-FROM class_subjects c
-JOIN subjects s ON c.subject_id = s.subject_id
-JOIN classes e ON c.class_id = e.class_id
-JOIN teachers t ON c.teacher_id = t.teacher_id
+FROM public.class_subjects c
+JOIN public.subjects s ON c.subject_id = s.subject_id
+JOIN public.classes e ON c.class_id = e.class_id
+JOIN public.teachers t ON c.teacher_id = t.teacher_id
 where c.teacher_id=$1`;
 
 const getclasses = `select exam_date,exam_id,id as exam_schedule_id,total_marks,exam_schedule.subject_id,exam_schedule.subject,classes.class_id,
-	classes.class_name,class_subjects.class_subject_id,class_subjects.teacher_id as subj_teacher from exam_schedule
-join class_subjects on class_subjects.subject_id=exam_schedule.subject_id
-	join classes on class_subjects.class_id=classes.class_id
+	classes.class_name,class_subjects.class_subject_id,class_subjects.teacher_id as subj_teacher from public.exam_schedule
+join public.class_subjects on class_subjects.subject_id=exam_schedule.subject_id
+	join public.classes on class_subjects.class_id=classes.class_id
 where exam_schedule.class_grade_id=classes.class_grade_id and class_subjects.teacher_id =$1 and exam_id=$2`;
 
 const attendancerecords = `SELECT 
@@ -40,10 +39,10 @@ const attendancerecords = `SELECT
     c.class_name,
     SUM(CASE WHEN a.status = 'Present' THEN 1 ELSE 0 END) AS present_students
 FROM 
-    attendance a
-	join students s on s.student_id=a.student_id
+    public.attendance a
+	join public.students s on s.student_id=a.student_id
 JOIN 
-    classes c ON s.class_id = c.class_id
+    public.classes c ON s.class_id = c.class_id
 WHERE 
     s.class_id = $1
 GROUP BY 
